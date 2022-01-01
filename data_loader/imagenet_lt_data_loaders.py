5a150f5784d00d80088121486e3000828358c673
UeHoME <r06921066@ntu.edu.tw>
2022-01-01 13:14:11 +0800

    modify

--- data_loader/imagenet_lt_data_loaders.py
+++ data_loader/imagenet_lt_data_loaders.py
@@ -1,11 +1,15 @@
 import torch
 import random
 import numpy as np
-import os, sys
+import os
+import sys
+import glob
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Dataset, Sampler
 from base import BaseDataLoader
 from PIL import Image
+import pandas as pd
+
 
 class BalancedSampler(Sampler):
     def __init__(self, buckets, retain_epoch_size=False):
@@ -16,7 +20,7 @@ class BalancedSampler(Sampler):
         self.buckets = buckets
         self.bucket_pointers = [0 for _ in range(self.bucket_num)]
         self.retain_epoch_size = retain_epoch_size
-    
+
     def __iter__(self):
         count = self.__len__()
         while count > 0:
@@ -35,12 +39,15 @@ class BalancedSampler(Sampler):
 
     def __len__(self):
         if self.retain_epoch_size:
-            return sum([len(bucket) for bucket in self.buckets]) # Actually we need to upscale to next full batch
+            # Actually we need to upscale to next full batch
+            return sum([len(bucket) for bucket in self.buckets])
         else:
-            return max([len(bucket) for bucket in self.buckets]) * self.bucket_num # Ensures every instance has the chance to be visited in an epoch
+            # Ensures every instance has the chance to be visited in an epoch
+            return max([len(bucket) for bucket in self.buckets]) * self.bucket_num
+
 
 class LT_Dataset(Dataset):
-    
+
     def __init__(self, root, txt, transform=None):
         self.img_path = []
         self.labels = []
@@ -49,37 +56,54 @@ class LT_Dataset(Dataset):
             for line in f:
                 self.img_path.append(os.path.join(root, line.split()[0]))
                 self.labels.append(int(line.split()[1]))
-        self.targets = self.labels # Sampler needs to use targets
-        
+        self.targets = self.labels  # Sampler needs to use targets
+
     def __len__(self):
         return len(self.labels)
-        
+
     def __getitem__(self, index):
 
         path = self.img_path[index]
         label = self.labels[index]
-        
+
         with open(path, 'rb') as f:
             sample = Image.open(f).convert('RGB')
-        
+
         if self.transform is not None:
             sample = self.transform(sample)
 
         # return sample, label, path
         return sample, label
 
-class ImageNetLTDataLoader(DataLoader):
+
+class test_Dataset(Dataset):
+    def __init__(self, root, transform):
+        self.transform = transform
+        self.fnames = glob.glob(os.path.join(root, '*'))
+        self.num_samples = len(self.fnames)
+
+    def __getitem__(self, idx):
+        fname = self.fnames[idx]
+        img = Image.open(fname).convert('RGB')
+        img = self.transform(img)
+        label = 0
+        return img, label
+
+    def __len__(self):
+        return self.num_samples
+
+
+class FoodLTDataLoader(DataLoader):
     """
     ImageNetLT Data Loader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, num_workers=1, training=True, balanced=False, retain_epoch_size=True, 
-                 train_txt="./data_txt/ImageNet_LT/ImageNet_LT_train.txt", 
-                 val_txt="./data_txt/ImageNet_LT/ImageNet_LT_val.txt", 
-                 test_txt="./data_txt/ImageNet_LT/ImageNet_LT_test.txt"):
+
+    def __init__(self, data_dir, batch_size, shuffle=True, num_workers=1, training=True, balanced=False, retain_epoch_size=True):
         train_trsfm = transforms.Compose([
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0),
+            transforms.ColorJitter(
+                brightness=0.4, contrast=0.4, saturation=0.4, hue=0),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
@@ -91,10 +115,15 @@ class ImageNetLTDataLoader(DataLoader):
         ])
 
         if training:
-            dataset = LT_Dataset(data_dir,  train_txt, train_trsfm)
-            val_dataset = LT_Dataset(data_dir, val_txt, test_trsfm)
-        else: # test
-            dataset = LT_Dataset(data_dir, test_txt, test_trsfm)
+            # dataset = LT_Dataset(data_dir,  train_txt, train_trsfm)
+            # val_dataset = LT_Dataset(data_dir, val_txt, test_trsfm)
+            dataset = datasets.DatasetFolder(os.path.join(data_dir, 'train'), loader=lambda x: Image.open(
+                x), extensions="jpg", transform=train_trsfm)
+            val_dataset = datasets.DatasetFolder(
+                os.path.join(data_dir, 'val'), loader=lambda x: Image.open(x), extensions="jpg", transform=test_trsfm)
+        else:  # test
+            # dataset = LT_Dataset(data_dir, test_txt, test_trsfm)
+            dataset = test_Dataset(data_dir, test_trsfm)
             val_dataset = None
 
         self.dataset = dataset
@@ -119,10 +148,11 @@ class ImageNetLTDataLoader(DataLoader):
                 sampler = BalancedSampler(buckets, retain_epoch_size)
                 shuffle = False
             else:
-                print("Test set will not be evaluated with balanced sampler, nothing is done to make it balanced")
+                print(
+                    "Test set will not be evaluated with balanced sampler, nothing is done to make it balanced")
         else:
             sampler = None
-        
+
         self.shuffle = shuffle
         self.init_kwargs = {
             'batch_size': batch_size,
@@ -130,10 +160,11 @@ class ImageNetLTDataLoader(DataLoader):
             'num_workers': num_workers
         }
 
-        super().__init__(dataset=self.dataset, **self.init_kwargs, sampler=sampler) # Note that sampler does not apply to validation set
+        # Note that sampler does not apply to validation set
+        super().__init__(dataset=self.dataset, **self.init_kwargs, sampler=sampler)
 
     def split_validation(self):
         # If you do not want to validate:
-        #return None
+        # return None
         # If you want to validate:
         return DataLoader(dataset=self.val_dataset, **self.init_kwargs)
